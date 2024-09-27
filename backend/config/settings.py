b54bfa4c3a2a540be848a633121edac93a3135cc
Presh Samuel <preshsamuel266@gmail.com>
2024-09-27 13:58:12 +0100

    add notesapp-final.onrender.com

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*.onrender.com', 'notesapp-2.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*.onrender.com', 'notesapp-final.onrender.com']
 
 
 # Application definition
