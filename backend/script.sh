bb2094f064e53ac5fe203bf03c221a0072dfc09f
Presh Samuel <preshsamuel266@gmail.com>
2024-09-27 14:03:01 +0100

    update script.sh

--- backend/script.sh
+++ backend/script.sh
@@ -2,4 +2,5 @@
 
 python3 manage.py migrate
 python manage.py collectstatic --no-input
-waitress-serve --listen=*:8000 config.wsgi:application
+# waitress-serve --listen=*:8000 config.wsgi:application
+gunicorn config.wsgi:application
