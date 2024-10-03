fc791c054e6c585cf9c1af8432593921b7e933f6
Presh Samuel <preshsamuel266@gmail.com>
2024-10-03 11:45:29 +0100

    Modified ALLOWED_HOST

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*.onrender.com', 'notesapp-1-c2sb.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*.onrender.com', 'notesapp-22kc.onrender.com']
 
 
 # Application definition
